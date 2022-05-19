b7f40858bba500569746bf7f79512224ec3ff1e8
Siyovush <494731@student.saxion.nl>
2022-05-19 14:44:49 +0200

    Added pre-login tests

--- frontend/cypress/integration/test.spec.js
+++ frontend/cypress/integration/test.spec.js
@@ -4,18 +4,43 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-describe('Test', function() {
-    it('Login', function () {
+
+describe('Pre-login section', function() {
+
+    // Proper login 
+    it('Proper Login', function () {
         cy.visit('localhost:4200')
         cy.get('input').type('realemail@gmail.com').should('have.value', 'realemail@gmail.com')
         cy.get('button').contains('LOGIN').click() 
     })
-})
 
-describe('Test', function() {
+    // Improper login
     it('Improper Login', function () {
         cy.visit('localhost:4200')
         cy.get('input').type('fake_login').should('have.value', 'fake_login')
         cy.get('button').contains('LOGIN').click()
     })
+
+    // Help button - Privacy statement
+    it('Help button - Privacy statement', function () {
+        cy.visit('localhost:4200')
+        cy.get('#question-button').click()
+        cy.get('button').contains('Privacy statement').click()
+    })
+
+
+    // Help button - About
+    it('Help button - About', function () {
+        cy.visit('localhost:4200')
+        cy.get('#question-button').click()
+        cy.get('button').contains('About').click()
+    })
+
+    // Help button - Report issue
+    it('Help button - Report issue', function () {
+        cy.visit('localhost:4200')
+        cy.get('#question-button').click()
+        cy.get('button').contains('Report issue').click()
+    })
+
 })
