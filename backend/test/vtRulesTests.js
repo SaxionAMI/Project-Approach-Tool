b929578f9843effe6b46869530f825f47a47a7d0
washedszn <henrywri2020@gmail.com>
2022-05-25 09:40:40 +0200

    Added template for vtRules tests

---
+++ backend/test/vtRulesTests.js
@@ -0,0 +1,92 @@
+const assert = require("assert");
+const request = require('supertest');
+
+// TODO - GET requests
+
+describe('GET/ vt-rules', () => {
+    it('should <enter>', () => {
+
+    })
+})
+
+describe('GET/ vt-rules/all', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/actions', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/conditions', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/comparisons', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/scopes', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/catalog-tabs', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/severities', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/phases', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('GET/ vt-rules/strategies', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+// TODO - POST requests
+
+describe('POST/ vt-rules', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('POST/ vt-rules/:_id/set-enabled', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+describe('POST/ vt-rules/:_id', () => {
+  it('should <enter>', () => {
+
+  })
+})
+
+// TODO - DELETE requests
+
+describe('DELETE/ vt-rules/:_id', () => {
+  it('should <enter>', () => {
+
+  })
+})
