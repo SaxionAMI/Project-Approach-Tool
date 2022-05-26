8892e111c66271f956d400af5f3896c68f000e6f
Yibing Chen <444980@student.saxion.nl>
2022-05-26 16:12:41 +0200

    test get deck by given id
    Signed-off-by: Yibing Chen <444980@student.saxion.nl>

---
+++ backend/test/deckTest.js
@@ -0,0 +1,35 @@
+const request = require("supertest")("http://localhost:13788");
+const auth=require("./auth");
+
+before(async ()=>{
+    global.token=await auth.generateToken("student");
+});
+
+
+// get a deck by id good weather
+describe("GET/ deck/:deckId", ()=>{
+    it("Should get a deck by given id", (done)=>{
+        request
+            .get("/deck/5e30276e8b5f721a0cc1f415")
+            .set("Authorization", global.token)
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    });
+});
+
+// get a deck by id bad weather
+describe("GET/ deck/:deckId", ()=>{
+    it("Should get a deck by given id", (done)=>{
+        request
+            .get("/deck/wrongid")
+            .set("Authorization", global.token)
+            .expect("Content-Type", /json/)
+            .expect(500, done);
+    });
+});
+
+
+
+
+
+
