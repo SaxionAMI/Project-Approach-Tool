399c7a09da286788b216579da7437deb51f3d38a
washedszn <henrywri2020@gmail.com>
2022-05-27 13:25:02 +0200

    Template for user tests

--- backend/test/userTests.js
+++ backend/test/userTests.js
@@ -1,6 +1,6 @@
-const {
-    before
-} = require("mocha");
+const { before } = require("mocha");
+const should = require('should');
+const assert = require('assert');
 const request = require('supertest')('http://localhost:13788');
 const auth = require('./auth');
 
@@ -10,14 +10,16 @@ before(async () => {
     global.adminToken = await auth.generateToken('admin', 'adminid');
 })
 
+// POST Tests
+
 describe('POST/ user', () => {
-    it('should create account', (done) => {
+    it('Should create account', (done) => {
         let body = {
-            firstName: 'adminfirstname',
-            lastName: 'adminlastname',
-            email: 'adminemail@projectapproachtool.nl',
-            study: 'adminstudy',
-            school: 'adminschool',
+            firstName: 'testfirstname',
+            lastName: 'testlastname',
+            email: 'test@projectapproachtool.nl',
+            study: 'teststrudy',
+            school: 'testschool',
             uid: 'adminid'
         }
         request
@@ -25,5 +27,53 @@ describe('POST/ user', () => {
             .set('Authorization', global.adminToken)
             .send(body)
             .expect(200, done)
+    })
+})
+
+describe('POST/ user/check', () => {
+    it('Should', (done) => {
+
+    })
+})
+
+describe('POST/ user/:uid/role', () => {
+    it('Should', (done) => {
+        
+    })
+})
+
+// GET Tests
+
+describe('GET/ user/role', () => {
+    it('Should', (done) => {
+        
+    })
+})
+
+describe('GET/ user/:uid', () => {
+    it('Should', (done) => {
+        
+    })
+})
+
+describe('GET/ user/export/:uid', () => {
+    it('Should', (done) => {
+        
+    })
+})
+
+// PUT Tests
+
+describe('PUT/ user/:uid', () => {
+    it('Should', (done) => {
+        
+    })
+})
+
+// DELETE Tests
+
+describe('DELETE/ user/:uid', () => {
+    it('Should', (done) => {
+        
     })
 })
