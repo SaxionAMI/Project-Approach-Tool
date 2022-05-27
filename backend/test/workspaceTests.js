2e727f43ecbfbe33a0785c0bd4110ce88a454671
Yibing Chen <444980@student.saxion.nl>
2022-05-27 07:33:08 +0200

    tests workspace upsert with existing workspace and non-existing workspace
    Signed-off-by: Yibing Chen <444980@student.saxion.nl>

--- backend/test/workspaceTests.js
+++ backend/test/workspaceTests.js
@@ -65,3 +65,78 @@ describe("GET /workspace/:_id", ()=>{
     });
 });
 
+
+// test upsert a workspace when no workspace exists with given id good weather
+describe("POST /workspace/:_id", ()=>{
+    it("Should create a workspace with given request body and the given id", (done)=>{
+        const body = {
+            _id: "62905d8d67e5b552986ddd16",
+            title: "sample title upsert first time",
+            goal: "sample goal upsert first time",
+            image: "../../../../../assets/image/whiteSmoke.PNG"
+        };
+        request
+            .post("/workspace/62905d8d67e5b552986ddd16")
+            .set("Authorization", global.token)
+            .send(body)
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    });
+});
+
+
+// test upsert a workspace when no workspace exists with given id bad weather
+describe("POST /workspace/:_id", ()=>{
+    it("Should not be able to create a workspace", (done)=>{
+        const body = {
+            _id: "62905d8d67e5b552986ddd16haha",
+            titttle: "sample title upsert first time",
+            goal: "sample goal upsert first time",
+            image: "../../../../../assets/image/whiteSmoke.PNG"
+        };
+        request
+            .post("/workspace/62905d8d67e5b552986ddd16haha")
+            .set("Authorization", global.token)
+            .send(body)
+            .expect("Content-Type", /json/)
+            .expect(500, done);
+    });
+});
+
+// test upsert a workspace when a workspace exists with given id good weather
+describe("POST /workspace/:_id", ()=>{
+    it("Should update a workspace with given request body and the given id", (done)=>{
+        const body = {
+            _id: "62905d8d67e5b552986ddd16",
+            title: "sample title upsert second time",
+            goal: "sample goal upsert second time",
+            image: "../../../../../assets/image/whiteSmoke.PNG"
+        };
+        request
+            .post("/workspace/62905d8d67e5b552986ddd16")
+            .set("Authorization", global.token)
+            .send(body)
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    });
+});
+
+// test upsert a workspace when a workspace exists with given id bad weather
+describe("POST /workspace/:_id", ()=>{
+    // eslint-disable-next-line max-len
+    it("Should not be able to update a workspace with given request body and the given id because the request body changes the workspace id", (done)=>{
+        const body = {
+            _id: "62905d8d67e5b552986ddd16hahahahah",
+            title: "sample title upsert second time wrong format",
+            goal: "sample goal upsert second time wrong format",
+            image: "../../../../../assets/image/whiteSmoke.PNG"
+        };
+        request
+            .post("/workspace/62905d8d67e5b552986ddd16")
+            .set("Authorization", global.token)
+            .send(body)
+            .expect("Content-Type", /json/)
+            .expect(500, done);
+    });
+});
+
