71897c7159fd26314268b150fd878620e7b6520d
Yibing Chen <444980@student.saxion.nl>
2022-05-27 07:18:51 +0200

    tests create a workspace Signed-off-by: Yibing Chen <444980@student.saxion.nl>

--- backend/test/workspaceTests.js
+++ backend/test/workspaceTests.js
@@ -6,4 +6,60 @@ before(async ()=>{
 });
 
 
+// test create a workspace good weather
+describe("POST /workspace", ()=>{
+    it("Should create a workspace with given request body", (done)=>{
+        const body = {
+            _id: "62905d8d67e5b552986ddd2b",
+            title: "sample title",
+            goal: "sample goal",
+            image: "../../../../../assets/image/whiteSmoke.PNG"
+        };
+        request
+            .post("/workspace")
+            .set("Authorization", global.token)
+            .send(body)
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    });
+});
+
+// test create a workspace bad weather
+describe("POST /workspace", ()=>{
+    it("Should not be able to create a workspace with given request body because the request body is the wrong format", (done)=>{
+        const body = {
+            _id: "62905d8d67e5b552986ddd2bhaha",
+            tittttle: "sample title",
+            goal: "sample goal",
+            image: "../../../../../assets/image/whiteSmoke.PNG"
+        };
+        request
+            .post("/workspace")
+            .set("Authorization", global.token)
+            .send(body)
+            .expect("Content-Type", /json/)
+            .expect(500, done);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+// test get workspace by id good weather
+describe("GET /workspace/:_id", ()=>{
+    it("Should get a workspace by given id", (done)=>{
+        request
+            .get("/deck/5e30276e8b5f721a0cc1f415")
+            .set("Authorization", global.token)
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    });
+});
 
