25f4cb44c59c6cbbadd7394437209be606675fec
Siyovush <494731@student.saxion.nl>
2022-05-29 18:13:12 +0200

    Additions to workplace manipulation

--- frontend/cypress/integration/post-login-tests/actions.spec.js
+++ frontend/cypress/integration/post-login-tests/actions.spec.js
@@ -4,6 +4,7 @@ before (() => {
 
 beforeEach(() => {
     cy.visit('localhost:4200')
+    cy.wait(1000)
 })
 
 describe('Post-login section', function() {
@@ -35,6 +36,7 @@ describe('Post-login section', function() {
         // figure out how to check if workplace is created
     })
 
+    // TODO: Get menu to become visible
     // Copy workplace
     it('Copy workplace', function () {
         cy.get('app-workspace-card')
@@ -46,7 +48,8 @@ describe('Post-login section', function() {
         cy.get('button').contains('Make copy').should('be.visible').click()
     })
 
-    //Delete workplace
+    // TODO: Get menu to become visible
+    // Delete workplace
     it('Delete workplace', function () {
         cy.get('app-workspace-card')
         cy.get('mat-card').eq(0)
@@ -57,12 +60,18 @@ describe('Post-login section', function() {
         cy.get('button').contains('Delete').should('be.visible').click()
     })
 
-    it('Enter workplace', function () {
+    it('Enter workplace and select decks', function () {
         cy.get('app-workspace-card')
         cy.get('.mat-card-image').eq(0).click({force:true})
-        // cy.get('div').eq(0).click()
-
-
+        cy.get('#mat-dialog-0').should('be.visible')
+        // Forcing true causes list to no longer be selected. why??
+        cy.get('li').click('left', { force:true , multiple: true})
+        cy.get('button').contains('SAVE').click({force:true})
+        cy.get('button').contains('build').click()
+        cy.get('#screen > mat-drawer > div > div.ng-star-inserted > img:nth-child(1)').should('be.visible').click()
+        cy.get('#screen > mat-drawer > div > div.ng-star-inserted > img:nth-child(2)').should('be.visible').click()
+        // somehow get the dynamic item and drag it onto the other one. 
+        cy.get('1653838874430').trigger('mousedown', 40, 40)
     })
 
 })
