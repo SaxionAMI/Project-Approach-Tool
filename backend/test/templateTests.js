41afae19f7b6e461c723491eb7c2bd9f0d98f6d6
Yibing Chen <444980@student.saxion.nl>
2022-05-30 15:10:33 +0200

    tests get example template good weather and unauth secenario
    Signed-off-by: Yibing Chen <444980@student.saxion.nl>

--- backend/test/templateTests.js
+++ backend/test/templateTests.js
@@ -25,3 +25,27 @@ describe("GET /template", ()=>{
             .expect(403, done);
     });
 });
+
+// test get example template
+describe("GET /template/example", ()=>{
+    it("Should get all example templates", (done)=>{
+        request
+            .get("/template/example")
+            .set("Authorization", global.token)
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    });
+});
+
+// test get example tempalte when unauthorized
+describe("GET /template/example", ()=>{
+    it("Should not get example templates", (done)=>{
+        request
+            .get("/template/example")
+            .expect(403, done);
+    });
+});
+
+
+
+
