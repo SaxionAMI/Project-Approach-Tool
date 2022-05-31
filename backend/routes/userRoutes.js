6aa5d96aa5078f29cc68a982e5aa6cc9087c3b2c
washedszn <henrywri2020@gmail.com>
2022-05-31 15:00:28 +0200

    Added template for user apidoc

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,20 +4,78 @@ const Router = Express.Router();
 const User = require("../controllers/userController");
 const authorization = require("../authorizationModule");
 
-//  The post and upsert routings
+/**
+ * @api {post} /user Request
+ * @apiName post
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.post("/user", User.postUser);
+
+/**
+ * @api {post} /user/check Request
+ * @apiName post
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.post("/user/check", User.checkIfValidEmail);
+
+/**
+ * @api {post} /user/:uid/role Request
+ * @apiName post
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.post("/user/:uid/role", authorization.hasRole(['admin']), User.setUserRole);
 
-//  The get routings
+/**
+ * @api {get} /user/role Request
+ * @apiName get
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.get("/user/role", authorization.hasRole(['admin']), User.getUserRoles);
+
+/**
+ * @api {get} /user/:uid Request
+ * @apiName get
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.get("/user/:uid", User.getUserByUid);
+
+/**
+ * @api {get} /user/export/:uid Request
+ * @apiName get
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.get("/user/export/:uid", User.getExportReadyUserData);
 
-//  The update routings
+
+/**
+ * @api {put} /user/:uid Request
+ * @apiName put
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.put("/user/:uid", User.updateUser);
 
-//  The delete routings
+
+/**
+ * @api {delete} /user/:uid Request
+ * @apiName delete
+ * @apiGroup user
+ *
+ * @apiSuccess {}
+ */
 Router.delete("/user/:uid", User.deleteUser);
 
 module.exports = Router;
