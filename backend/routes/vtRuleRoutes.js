df357f61c78480a778fc2d500bb6fa73af257e44
washedszn <henrywri2020@gmail.com>
2022-05-31 14:57:41 +0200

    Added template for vt rules apidoc

--- backend/routes/vtRuleRoutes.js
+++ backend/routes/vtRuleRoutes.js
@@ -4,21 +4,131 @@ const Router = Express.Router();
 const vtRules = require("../controllers/vtFeedbackRuleController");
 const authorization = require("../authorizationModule");
 
+/**
+ * @api {get} /vt-rules Request
+ * @apiName getEnabledRules
+ * @apiGroup vtRules
+ *
+ * @apiSuccess {Object[]} rules     List of enabled rules.
+ * @apiSuccess {}
+ */
 Router.get("/vt-rules", vtRules.getEnabledRules);
+
+/**
+ * @api {get} /vt-rules/all Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/all", authorization.hasRole(['teacher','admin']), vtRules.getRules);
+
+/**
+ * @api {get} /vt-rules/actions Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/actions", authorization.hasRole(['teacher','admin']), vtRules.getActions);
+
+/**
+ * @api {get} /vt-rules/conditions Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/conditions", authorization.hasRole(['teacher','admin']), vtRules.getConditions);
+
+/**
+ * @api {get} /vt-rules/comparisons Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/comparisons", authorization.hasRole(['teacher','admin']), vtRules.getComparisons);
+
+/**
+ * @api {get} /vt-rules/scopes Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/scopes", authorization.hasRole(['teacher','admin']), vtRules.getScopes);
+
+/**
+ * @api {get} /vt-rules/catalog-tabs Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/catalog-tabs", authorization.hasRole(['teacher','admin']), vtRules.getCatalogTabs);
+
+/**
+ * @api {get} /vt-rules/severities Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/severities", authorization.hasRole(['teacher','admin']), vtRules.getSeverities);
+
+/**
+ * @api {get} /vt-rules/phases Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/phases", authorization.hasRole(['teacher','admin']), vtRules.getPhases);
+
+/**
+ * @api {get} /vt-rules/strategies Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.get("/vt-rules/strategies", authorization.hasRole(['teacher','admin']), vtRules.getStrategies);
 
+/**
+ * @api {post} /vt-rules Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.post("/vt-rules", authorization.hasRole(['teacher','admin']), vtRules.createRule);
+
+/**
+ * @api {post} /vt-rules/:_id Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.post("/vt-rules/:_id", authorization.hasRole(['teacher','admin']), vtRules.updateRule);
+
+/**
+ * @api {post} /vt-rules/:_id/set-enabled Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.post("/vt-rules/:_id/set-enabled", authorization.hasRole(['teacher','admin']), vtRules.setRuleEnabled);
 
+/**
+ * @api {delete} /vt-rules/:_id Request
+ * @apiName get
+ * @apiGroup vtRules
+ *
+ * @apiSuccess 
+ */
 Router.delete("/vt-rules/:_id", authorization.hasRole(['teacher','admin']), vtRules.deleteRule);
 
 module.exports = Router;
