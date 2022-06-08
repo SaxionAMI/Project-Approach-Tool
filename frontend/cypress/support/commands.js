81b676820abd22b0fa04fc5c6d07841bfc86c5cd
Siyovush <494731@student.saxion.nl>
2022-06-08 11:21:22 +0200

    Remove drag-drop leftover from commands

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -24,7 +24,6 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import "@4tw/cypress-drag-drop";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
