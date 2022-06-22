47e38fda7ae2bae89fffde8d0b53b3d5b8410c0d
washedszn <henrywri2020@gmail.com>
2022-06-22 08:45:18 +0200

    added to auto deploy scrips

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
-git pull origin main
-cd frontend/
-npm install
-cd ..
+git pull origin development
 cd backend/
 npm install
+cd ..
+cd frontend/
+npm install
+npx ng build --prod
+rm -rf /var/www/projectapproachtool/html
+mv dist/PAT/* /var/www/projectapproachtool/html
