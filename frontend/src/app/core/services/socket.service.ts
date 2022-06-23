8fc19221da898ac4233adec1cd1aa8485e02e0ee
FewFaces012 <495673@student.saxion.nl>
2022-06-23 14:18:06 +0200

    Fixed the view of actions performed from another user on the first user screen (card movement)

--- frontend/src/app/core/services/socket.service.ts
+++ frontend/src/app/core/services/socket.service.ts
@@ -87,8 +87,8 @@ export class SocketService {
    * @param  {Card} card - the card object
    * @returns void
    */
-  moveCardToGroup(room: string, card: Card, workspace: VTWorkspaceData): void  {
-    this.socket.emit("moveCardToGroup", { room, data: card.data(), workspace: workspace });
+  moveCardToGroup(room: string, value: any, workspace: VTWorkspaceData): void  {
+    this.socket.emit("moveCardToGroup", { room, data: value, workspace: workspace});
   }
 
   /**
