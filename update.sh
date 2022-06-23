187381b92f96d3405bd493179f3baedd0afe288e
Ubuntu <azuremanage@PATV2.nwt4n0qkzysupi3ylaygv3x33d.ax.internal.cloudapp.net>
2022-06-23 14:21:00 +0000

    fixed update file

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 git pull origin development
-cd frontend/
-npm install
-cd ..
 cd backend/
 npm install
+cd ..
+cd frontend/
+npm install
+npx ng build --prod
+rm -rf /var/www/projectapproachtool/html
+mv dist/PAT/* /var/www/projectapproachtool/html
+
